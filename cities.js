699d9965126150933a462736027ccbce784b461a
mohamed hussien <151773578+mmineawy@users.noreply.github.com>
2023-12-02 20:58:49 +0200

    Add files via upload

---
+++ cities.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const citiesElements = document.querySelectorAll('.cities-w');
+
+    
+    const detElements = document.querySelectorAll('.det');
+
+    
+    citiesElements.forEach((cityElement, index) => {
+        cityElement.addEventListener('click', function () {
+            
+            detElements.forEach(detElement => {
+                detElement.style.display = 'none';
+            });
+
+            
+            detElements[index].style.display = 'block';
+        });
+    });
+});
